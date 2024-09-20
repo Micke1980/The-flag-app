5697fde2cc4068637dfe470796fe86d2f0fae24e
Micke1980 <tm_mike@hotmail.com>
2024-09-20 21:10:31 +0200

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData, useSearchParams, useNavigation } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import Search from '../components/Search';
 import Filter from '../components/Filter';
 import Box from '@mui/material/Box';
@@ -10,44 +10,37 @@ import SkeletonCard from '../components/SkeltonCard';
 import { ColorModeContext } from '../components/App';
 
 export default function Home() {
-    const { loadingOverride } = useContext(ColorModeContext);
-    const [searchParams, setSearchParams] = useSearchParams(); 
-    const dataArray = useLoaderData();
-    const { state } = useNavigation();
-    const searchTerm = searchParams.get('search') || '';
-    const region = searchParams.get('region') || '';
+	const { loadingOverride } = useContext(ColorModeContext);
 
-    const filteredData = dataArray
-        .filter(country => 
-            country.name.common.toLowerCase().includes(searchTerm.toLowerCase()) &&
-            (region ? country.region === region : true)
-        )
-        .sort(sortAlphabetically);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const dataArray = useLoaderData();
+	const sorted = dataArray.sort(sortAlphabetically);
+	const { state } = useNavigation();
 
-    return (
-        <Box className="Home" pt={4}>
-            <Grid container mb={4} justifyContent="space-between" spacing={2}>
-                <Grid item xs={12} md={4}>
-                    <Search setSearchParams={setSearchParams} />
-                </Grid>
-                <Grid item xs={6} md={2}>
-                    <Filter setSearchParams={setSearchParams} />
-                </Grid>
-            </Grid>
+	return (
+		<Box className="Home" pt={4}>
+			<Grid container mb={4} justifyContent="space-between" spacing={2}>
+				<Grid item md={4} xs={12}>
+					<Search setSearchParams={setSearchParams} />
+				</Grid>
+				<Grid item md={2} xs={6}>
+					<Filter setSearchParams={setSearchParams} />
+				</Grid>
+			</Grid>
 
-            <Grid
-                container
-                spacing={4}
-                rowGap={4}
-                flexDirection={{ xs: 'column', md: 'row' }}
-                alignContent={{ xs: 'center', md: 'start' }}
-            >
-                {state === 'loading' || loadingOverride
-                    ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, i) => <SkeletonCard key={i} />)
-                    : filteredData.map((country, i) => <CountryCard country={country} key={i} />)}
-            </Grid>
-        </Box>
-    );
+			<Grid
+				container
+				spacing={4}
+				rowGap={4}
+				flexDirection={{ xs: 'column', md: 'row' }}
+				alignContent={{ xs: 'center', md: 'start' }}
+			>
+				{state === 'loading' || loadingOverride
+					? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, i) => <SkeletonCard key={i} />)
+					: sorted.map((country, i) => <CountryCard country={country} key={i} />)}
+			</Grid>
+		</Box>
+	);
 }
 
 async function getAllCountrys() {
